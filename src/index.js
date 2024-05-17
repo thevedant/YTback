16824561e7499724e71cc722104b3a518e36dc6d
Vedant Gupta <thevedantgupta@gmail.com>
2024-05-17 19:38:05 +0530

    Adding utilities

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
     path:'./.env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("DB failed !!!" , err);
+})
 
 
 
