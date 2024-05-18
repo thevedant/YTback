dd77464955dd28c37ece276e9d80733b7e09e61f
Vedant Gupta <thevedantgupta@gmail.com>
2024-05-18 09:53:13 +0530

    som err resolved

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import express from "express"
+
+const app = express()
 
 dotenv.config({
     path:'./.env'
