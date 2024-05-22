221aee00aa18d422739c689cc483dbf00e130f70
Vedant Gupta <thevedantgupta@gmail.com>
2024-05-22 20:00:22 +0530

    Add subscription schema

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose , { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type : Schema.Types.ObjectId, //the one who is subscribing
+        ref : "User"
+    },
+    channel:{
+        type : Schema.Types.ObjectId, //the one to whom one is subscribing
+        ref : "User"
+    }
+}, { timestamps }
+)
+
+
+export const Subscription = mongoose.model("Subscription" , subscriptionSchema);
